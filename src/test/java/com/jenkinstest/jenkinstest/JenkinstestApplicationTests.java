46e72df0c9599ea2c39c571c8dfdae5d9500fb7b
Pravallika Cherukuri <pravallikacherukuri@Pravallikas-MBP.lan>
2022-07-07 15:27:10 -0500

    added a test case

--- src/test/java/com/jenkinstest/jenkinstest/JenkinstestApplicationTests.java
+++ src/test/java/com/jenkinstest/jenkinstest/JenkinstestApplicationTests.java
@@ -1,13 +1,22 @@
 package com.jenkinstest.jenkinstest;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class JenkinstestApplicationTests {
 
 	@Test
 	void contextLoads() {
+		TestController homeController = new TestController();
+		String result = homeController.getHelloWorld();
+		assertEquals(result, "Helloooo Andy.. How are you doing today???");
 	}
 
 }
